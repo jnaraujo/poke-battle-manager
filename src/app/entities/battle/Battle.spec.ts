2c902c8ccedac3a6ff0a7f988be80be186f2bacc
jonatas araujo <jonatasaraujo2257@gmail.com>
2023-04-26 12:18:58 -0300

    ✨ feat(entities): add Battle

---
+++ src/app/entities/battle/Battle.spec.ts
@@ -0,0 +1,87 @@
+import { beforeEach, describe, expect, it } from "vitest";
+import { Battle } from "./Battle";
+import { Trainer } from "../trainer/Trainer";
+
+describe("Battle", () => {
+  let battle: Battle;
+  let trainer1: Trainer;
+  let trainer2: Trainer;
+
+  beforeEach(() => {
+    trainer1 = new Trainer({
+      id: "1",
+      name: "Ash",
+      city: "Pallet",
+      age: 10,
+      level: 1,
+      pokemons: [],
+      items: [],
+      league: "Kanto",
+    });
+
+    trainer2 = new Trainer({
+      id: "2",
+      name: "Misty",
+      city: "Cerulean",
+      age: 10,
+      level: 1,
+      pokemons: [],
+      items: [],
+      league: "Kanto",
+    });
+
+    battle = new Battle({
+      id: "1",
+      trainer1,
+      trainer2,
+      createdAt: new Date(),
+      startedAt: null,
+      finishedAt: null,
+      winner: null,
+      loser: null,
+    });
+  });
+
+  it("should start a battle", () => {
+    expect(battle.startedAt).toBeNull();
+
+    battle.start();
+    expect(battle.startedAt).not.toBeNull();
+  });
+
+  it("should finish a battle", () => {
+    expect(battle.finishedAt).toBeNull();
+    expect(battle.winner).toBeNull();
+    expect(battle.loser).toBeNull();
+
+    battle.finish(trainer1, trainer2);
+    expect(battle.finishedAt).not.toBeNull();
+    expect(battle.winner).toEqual(trainer1);
+    expect(battle.loser).toEqual(trainer2);
+  });
+
+  it("should check if a battle is finished", () => {
+    expect(battle.isFinished()).toBe(false);
+
+    battle.finish(trainer1, trainer2);
+    expect(battle.isFinished()).toBe(true);
+  });
+
+  it("should check if a battle is equal to another battle", () => {
+    const battle2 = new Battle({
+      id: "2",
+      trainer1,
+      trainer2,
+      createdAt: new Date(),
+      startedAt: null,
+      finishedAt: null,
+      winner: null,
+      loser: null,
+    });
+
+    expect(battle.equals(battle2)).toBe(false);
+
+    battle2.id = "1";
+    expect(battle.equals(battle2)).toBe(true);
+  });
+});
---
+++ src/app/entities/battle/Battle.ts
@@ -0,0 +1,118 @@
+import { Trainer } from "../trainer/Trainer";
+
+export class Battle {
+  _id: string;
+  _trainer1: Trainer;
+  _trainer2: Trainer;
+  _winner: Trainer | null;
+  _loser: Trainer | null;
+  _createdAt: Date | null;
+  _startedAt: Date | null;
+  _finishedAt: Date | null;
+
+  constructor(props: {
+    id: string;
+    trainer1: Trainer;
+    trainer2: Trainer;
+    winner: Trainer | null;
+    loser: Trainer | null;
+    createdAt: Date | null;
+    startedAt: Date | null;
+    finishedAt: Date | null;
+  }) {
+    this._id = props.id;
+    this._trainer1 = props.trainer1;
+    this._trainer2 = props.trainer2;
+    this._createdAt = props.createdAt;
+    this._startedAt = props.startedAt;
+    this._finishedAt = props.finishedAt;
+    this._winner = props.winner;
+    this._loser = props.loser;
+  }
+
+  // Methods
+
+  start(): void {
+    this._startedAt = new Date();
+  }
+
+  finish(winner: Trainer, loser: Trainer): void {
+    this._finishedAt = new Date();
+    this._winner = winner;
+    this._loser = loser;
+  }
+
+  isFinished(): boolean {
+    return this._finishedAt !== null;
+  }
+
+  equals(battle: Battle): boolean {
+    return this._id === battle.id;
+  }
+
+  // getters and setters
+
+  get id(): string {
+    return this._id;
+  }
+
+  set id(id: string) {
+    this._id = id;
+  }
+
+  get trainer1() {
+    return this._trainer1;
+  }
+
+  set trainer1(trainer1: Trainer) {
+    this._trainer1 = trainer1;
+  }
+
+  get trainer2() {
+    return this._trainer2;
+  }
+
+  set trainer2(trainer2: Trainer) {
+    this._trainer2 = trainer2;
+  }
+
+  get winner() {
+    return this._winner;
+  }
+
+  set winner(winner: Trainer | null) {
+    this._winner = winner;
+  }
+
+  get loser() {
+    return this._loser;
+  }
+
+  set loser(loser: Trainer | null) {
+    this._loser = loser;
+  }
+
+  get createdAt() {
+    return this._createdAt;
+  }
+
+  set createdAt(createdAt: Date | null) {
+    this._createdAt = createdAt;
+  }
+
+  get startedAt() {
+    return this._startedAt;
+  }
+
+  set startedAt(startedAt: Date | null) {
+    this._startedAt = startedAt;
+  }
+
+  get finishedAt() {
+    return this._finishedAt;
+  }
+
+  set finishedAt(finishedAt: Date | null) {
+    this._finishedAt = finishedAt;
+  }
+}
