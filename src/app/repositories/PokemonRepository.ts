c45da736da076c500d8fa2bca153392b667e24ad
jonatas araujo <jonatasaraujo2257@gmail.com>
2023-04-26 13:26:35 -0300

    ✨ feat(PokemonRepository): add findByTrainerId

--- src/app/repositories/PokemonRepository.ts
+++ src/app/repositories/PokemonRepository.ts
@@ -1,4 +1,6 @@
 import { Pokemon } from "../entities/pokemon/Pokemon";
 import { Repository } from "./Repository";
 
-export interface PokemonRepository extends Repository<Pokemon> {}
+export interface PokemonRepository extends Repository<Pokemon> {
+  findByTrainerId(trainerId: string): Promise<Pokemon[]>;
+}
