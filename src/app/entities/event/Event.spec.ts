7efaf9d181a30b20a631f81c75a72dbaf5228d65
jonatas araujo <jonatasaraujo2257@gmail.com>
2023-04-26 12:57:22 -0300

    ✨ feat(entities): add Event

---
+++ src/app/entities/event/Event.spec.ts
@@ -0,0 +1,98 @@
+import { beforeEach, describe, expect, it } from "vitest";
+import { League } from "../league/League";
+import { Trainer } from "../trainer/Trainer";
+import { Battle } from "../battle/Battle";
+import { Event } from "./Event";
+
+describe("Event", () => {
+  let event: Event;
+  let league: League;
+  let trainer1: Trainer;
+  let trainer2: Trainer;
+  let battle: Battle;
+
+  beforeEach(() => {
+    trainer1 = new Trainer({
+      id: "1",
+      name: "Ash",
+      city: "Pallet Town",
+      age: 18,
+      level: 1,
+      pokemons: [],
+      items: [],
+      league: "Kanto",
+    });
+
+    trainer2 = new Trainer({
+      id: "2",
+      name: "Misty",
+      city: "Cerulean City",
+      age: 20,
+      level: 1,
+      pokemons: [],
+      items: [],
+      league: "Kanto",
+    });
+
+    league = new League({
+      id: "1",
+      battles: [battle],
+      trainers: [trainer1, trainer2],
+      registrationFee: 100,
+      prize: 1000,
+    });
+
+    event = new Event({
+      id: "1",
+      name: "Poke Global Event",
+      description: "Poke Global Event is a global event for all trainers",
+      leagues: [league],
+      trainers: [trainer1, trainer2],
+    });
+  });
+
+  it("should start an event", () => {
+    expect(event.startedAt).toBeNull();
+
+    event.start();
+    expect(event.startedAt).not.toBeNull();
+  });
+
+  it("should finish an event", () => {
+    expect(event.finishedAt).toBeNull();
+
+    event.finish();
+    expect(event.finishedAt).not.toBeNull();
+  });
+
+  it("should check if an event is finished", () => {
+    expect(event.isFinished()).toBeFalsy();
+
+    event.finish();
+    expect(event.isFinished()).toBeTruthy();
+  });
+
+  it("should check if an event is equal to another event", () => {
+    const event2 = new Event({
+      id: "1",
+      name: "Poke Global Event",
+      description: "Poke Global Event is a global event for all trainers",
+      leagues: [league],
+      trainers: [trainer1, trainer2],
+    });
+
+    expect(event.equals(event2)).toBeTruthy();
+  });
+
+  it("should check if an event is not equal to another event", () => {
+    const event2 = new Event({
+      id: "2",
+      name: "Poke Global Event",
+      description: "Poke Global Event is a global event for all trainers",
+      leagues: [league],
+      trainers: [trainer1, trainer2],
+    });
+
+    expect(event.equals(event2)).toBeFalsy();
+  });
+});
---
+++ src/app/entities/event/Event.ts
@@ -0,0 +1,115 @@
+import { League } from "../league/League";
+import { Trainer } from "../trainer/Trainer";
+
+export class Event {
+  _id: string;
+  _name: string;
+  _description: string;
+  _createdAt: Date | null;
+  _startedAt: Date | null;
+  _finishedAt: Date | null;
+  _trainers: Trainer[];
+  _leagues: League[];
+
+  constructor(props: {
+    id: string;
+    name: string;
+    description: string;
+    trainers: Trainer[];
+    leagues: League[];
+  }) {
+    this._id = props.id;
+    this._name = props.name;
+    this._description = props.description;
+    this._trainers = props.trainers;
+    this._leagues = props.leagues;
+
+    this._createdAt = null;
+    this._startedAt = null;
+    this._finishedAt = null;
+  }
+
+  // Methods
+
+  start() {
+    this._startedAt = new Date();
+  }
+
+  finish() {
+    this._finishedAt = new Date();
+  }
+
+  isFinished() {
+    return this._finishedAt !== null;
+  }
+
+  equals(event: Event) {
+    return this._id === event.id;
+  }
+
+  // getters and setters
+
+  get id() {
+    return this._id;
+  }
+
+  set id(id: string) {
+    this._id = id;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name: string) {
+    this._name = name;
+  }
+
+  get description() {
+    return this._description;
+  }
+
+  set description(description: string) {
+    this._description = description;
+  }
+
+  get createdAt() {
+    return this._createdAt;
+  }
+
+  set createdAt(createdAt: Date | null) {
+    this._createdAt = createdAt;
+  }
+
+  get startedAt() {
+    return this._startedAt;
+  }
+
+  set startedAt(startedAt: Date | null) {
+    this._startedAt = startedAt;
+  }
+
+  get finishedAt() {
+    return this._finishedAt;
+  }
+
+  set finishedAt(finishedAt: Date | null) {
+    this._finishedAt = finishedAt;
+  }
+
+  get trainers() {
+    return this._trainers;
+  }
+
+  set trainers(trainers: Trainer[]) {
+    this._trainers = trainers;
+  }
+
+  get battles() {
+    return this._leagues;
+  }
+
+  set battles(battles: League[]) {
+    this._leagues = battles;
+  }
+}
